33e9d3de6faf562b2f3fe2161c45da64f4726f78
Ruben Dietl <ruben.dietl@adamhall.com>
2023-09-23 14:07:00 +0200

    Jump animation, obstacle detection (let cleancode = false)

--- js/CoinClimb.js
+++ js/CoinClimb.js
@@ -1,46 +1,129 @@
 let canvas = document.getElementById("gameCanvas");
 let ctx = canvas.getContext("2d");
-//const startingPoint = document.getElementById("startingPoint");
 
-let playerOne = new Player();
-let startingObstacle = new Obstacles("startingPoint", 600, 375, 460, 260);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+let startingObstacle = new Obstacles("startingPoint", 1350, 650, 460, 260);
+let firstBaseObstacle = new Obstacles("firstBasePoint", 300, 575, 460, 260);
+let allObstacles = [startingObstacle, firstBaseObstacle]
+let playerOne = new Player(allObstacles);
+
+let keysPressed = [];
+
+let isWalkingLeft = false;
+let isWalkingRight = false;
+
+
+// KEYDOWN EVENT LISTENER
 document.addEventListener("keydown", function(event) {
+    //console.log("Player Y",playerOne.playerPosY+playerOne.player.clientHeight, "ObstacleY", startingObstacle.obstaclePosY, "ObstacleWidth", startingObstacle.obstacleHeight)
+    console.log("onObstacle",playerOne.onObstacle)
     switch(event.code) {
         case "Space":
-            playerOne.jump();
+            if(!playerOne.isJumping) { //in order to not spam the jump
+                playerOne.jump(0,0,playerOne.playerPosY);
+            }
             break;
         case "KeyA":
-            playerOne.walk("l");
+        case "ArrowLeft":
+            isWalkingLeft = true;
             break;
         case "KeyD":
-            playerOne.walk("r");            
+        case "ArrowRight":
+            isWalkingRight = true;
+            break;
+        default:
+            break;
+    }
+
+});
+
+document.addEventListener("keyup", function(event) {
+    keysPressed = []
+
+    switch(event.code) {
+        case "KeyA":
+        case "ArrowLeft":
+            isWalkingLeft = false;
+            break;
+        case "KeyD":
+        case "ArrowRight":
+            isWalkingRight = false;
+            //playerOne.instantStop = true;
             break;
         default:
             break;
     }
 });
 
-function initCanvas() {
-    // Clear Canvas before drawind
+// Function to draw the player
+function drawPlayer(player) {
+    if (player.facingRight) {
+        ctx.drawImage(player.player, player.playerPosX, Math.round(player.playerPosY), player.player.width, player.player.height);
+    } else {
+        drawFlippedImage(player.player, player.playerPosX, player.playerPosY, player.player.width, player.player.height);
+    }
+}
+
+// Function to draw flipped image
+function drawFlippedImage(image, x, y, width, height) {
+    ctx.save();
+    ctx.translate(x + width, y);
+    ctx.scale(-1, 1);
+    ctx.drawImage(image, 150, 0, width, height);
+    ctx.restore();
+}
+
+// Function to draw an obstacle
+function drawObstacle(obstacle) {
+    ctx.drawImage(obstacle.obstacle, obstacle.obstaclePosX, obstacle.obstaclePosY, obstacle.obstacleWidth, obstacle.obstacleHeight);
+}
+
+function gameLoop() {
+    isWalkingLeft && playerOne.walk("l");
+    isWalkingRight && playerOne.walk("r");
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw player dependent on facingRight
-    if (playerOne.facingRight) {
-        ctx.drawImage(playerOne.player, playerOne.playerPosX, playerOne.playerPosY, playerOne.player.width, playerOne.player.height);
-    } else {
-        ctx.save();
-        ctx.translate(playerOne.playerPosX + playerOne.player.width, playerOne.playerPosY);
-        ctx.scale(-1, 1);
-        ctx.drawImage(playerOne.player, 130, 0, playerOne.player.width, playerOne.player.height);
-        ctx.restore();
-    }
+    drawPlayer(playerOne);
+    drawObstacle(startingObstacle);
+    drawObstacle(firstBaseObstacle);
+    playerOne.isDescending = playerOne.jumpCount > 30
 
-    // Draw obstacle
-    ctx.drawImage(startingObstacle.obstacle, startingObstacle.obstaclePosX, startingObstacle.obstaclePosY, startingObstacle.obstacleWidth, startingObstacle.obstacleHeight);
-
-    requestAnimationFrame(initCanvas);
+    checkObjectCollision(playerOne.playerPosX, playerOne.player.clientWidth, playerOne.playerPosY, allObstacles);
+    
+    requestAnimationFrame(gameLoop);
 }
+
+
+function checkObjectCollision(playerPosX, playerWidth, playerPosY, allObstacles){
+    for (let i = 0; i < allObstacles.length; i++){
+        if((playerPosX + playerWidth) >= (allObstacles[i].obstaclePosX + playerWidth / 1.2) && (playerPosX <= (allObstacles[i].obstaclePosX + (allObstacles[i].obstacleWidth / 1.5)))){
+            if(playerOne.isDescending && playerPosY + playerOne.player.clientHeight <= allObstacles[i].obstaclePosY){
+                //playerOne.instantStop = true;
+                playerOne.onObstacle = true;
+                playerOne.targetLandingY = allObstacles[i].obstaclePosY - playerOne.player.clientHeight + 30;  // Storing the Y-coordinate for landing
+                playerOne.onTopY = allObstacles[i].obstaclePosY + 30;
+                //console.log("ON TOP", playerOne.onObstacle)
+            }
+        } else if(false){
+            
+
+
+        } else if(allObstacles[i].obstaclePosY === playerOne.playerPosY + playerOne.player.clientHeight -30){ // makes sure, when he leaves the obstacle, not to float in the air
+            playerOne.playerPosY = playerOne.canvasBottom;
+            playerOne.onObstacle = false;
+            //console.log("should be bottom now")
+        }
+    }
+}
+
+
+// obstaclePosY 650 PlayerOne Y: 650
+// if they are the same, I can just say he still is on a obstacle. If not anymore, set onObstacle to false and let him fall 
+
+
 window.onload = () => {
-    initCanvas();
+    gameLoop();
 }
--- js/Player.js
+++ js/Player.js
@@ -1,43 +1,88 @@
 class Player {
-    constructor(){
-        this.playerPosX = 270
-        this.playerPosY = 200
-        this.facingRight = true;
+    constructor(allObstacles){
         this.player = document.getElementById("playerOne");
+        this.canvas = document.getElementById("gameCanvas");
+        this.canvasBottom = this.canvas.clientHeight - this.player.clientHeight
+        this.playerPosX = this.canvas.clientWidth / 2
+        this.playerPosY = this.canvasBottom
+        this.facingRight = true;
+        this.walkingSpeed = 8;
+        this.allObstacles = allObstacles;
+
+        this.isJumping = false;
+        this.maxJumpHeight = 400
+        this.jumpCount = 0;
+        this.isDescending = this.jumpCount > 30;
+        this.jumpFrame = 1;
+
+        this.onObstacle = false;
+        this.onTopY = null;
+        this.instantStop = false;
+        this.targetLandingY = undefined;
     }
 
     walk(direction) {
         if(direction === "l") {
-            this.playerPosX -= 10;
+            this.playerPosX -= this.walkingSpeed;
             this.facingRight = false;
         } else if(direction === "r") {
-            this.playerPosX += 10;
+            this.playerPosX += this.walkingSpeed;
             this.facingRight = true;
         }
     }
 
     // A 60 FPS jump animation smoothed with a sine function
-    jump() {
-        if (60 <= 0) return;
-    
+    jump(right, left, startingPosY = this.canvasBottom) {
         const intervalTime = 500 / 60;
-        let count = 0;
-    
+        
+
+
         const interval = setInterval(() => {
-            this.playerPosY = 200 - 100 * Math.sin(count * Math.PI / 60);
+            this.isJumping = true;
+
+            if (this.targetLandingY !== undefined) {
+                const remainingJumpDuration = 60 - this.jumpCount;
+                const deltaY = this.targetLandingY - this.playerPosY;
+                const adjustedYMovement = deltaY / remainingJumpDuration;
     
-            count++;
-            if (count >= 60) {
-                clearInterval(interval);
+                this.playerPosY += adjustedYMovement;
+            } else {
+                this.playerPosY = startingPosY - this.maxJumpHeight * Math.sin(this.jumpCount * Math.PI / 60);
+            }
+
+            
+
+            this.playerPosX += right;
+            this.playerPosX -= left;
+            this.jumpCount++;
+            this.isDescending = this.jumpCount > 30
+            this.jumpAnimation(this.jumpCount);
+            if (this.jumpCount > 60) {
+                this.targetLandingY = undefined;
+                stopTheInterval()
             }
         }, intervalTime);
+
+        const stopTheInterval = () => {
+            this.isJumping = false;
+            this.player.src = `../images/player_one.png`
+            this.jumpFrame = 1;
+            this.jumpCount = 0;
+            clearInterval(interval);
+            if(this.onObstacle){
+                this.playerPosY = this.onTopY - this.player.clientHeight;
+            }
+        }
     }
 
-    leftJump(){
-        
+    //changing the images based on the jumpFrame
+    jumpAnimation(count) {
+        if(count % 10 === 0 && this.jumpFrame < 6) {
+            this.jumpFrame += 1;
+            this.player.src = `../images/jump/j${this.jumpFrame}.png`
+        }
     }
 
-    rightJump(){
 
-    }
+    
 }
